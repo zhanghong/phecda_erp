b3f1179b4d970884594995c969511bb9f093e96c
zhanghong <zhanghong@networking.io>
2013-11-12 11:31:11 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PhecdaErp::Application.config.secret_key_base = '09ea8f371580f163f8d1ae90df95dc74ca9e99cb17e32bc81c56d95e87610aa6e419aaf44df88f25fe144d2b6c143c7b943b18fccbface68d9298a95b27764c2'
